69ef6b3a27d7828de6d64422e12e03160b848b33
Nikita Pavlov <nikita_pavlov_ln@mail.ru>
2022-11-14 20:02:45 +0300

    Task2 сделан

--- src/ru/croc/task2/Task2.java
+++ src/ru/croc/task2/Task2.java
@@ -1,4 +1,29 @@
 package src.ru.croc.task2;
 
+import java.util.Scanner;
+
 public class Task2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //начальный аргумент
+        int a = scanner.nextInt();
+
+        //разность арифметической прогрессии
+        int b = scanner.nextInt();
+
+        //кол-во членов погрессии
+        int c = scanner.nextInt();
+
+        //сумма ряда
+        int sum = 0;
+
+        for(int i = 0; i < c; i++){
+            sum = sum + a;
+            a = a + b;
+        }
+        System.out.println("Sum: " + sum);
+
+    }
 }
