b7750beafc8e5a6568d3df3325e2908796cb8c34
KolaYAndr <nikolay_andreev02@mail.ru>
2022-11-15 17:21:02 +0300

    Task8 сделал

--- src/ru/croc/task8/Task8.java
+++ src/ru/croc/task8/Task8.java
@@ -1,4 +1,22 @@
 package ru.croc.task8;
 
+import java.text.NumberFormat;
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Task8 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a double: ");
+        try {
+            Double prize = scanner.nextDouble();
+            NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
+            System.out.println("Result: " + format.format(prize));
+        } catch (InputMismatchException e) {
+            System.err.println(e.getMessage() + " invalid number");
+        }
+
+    }
 }
