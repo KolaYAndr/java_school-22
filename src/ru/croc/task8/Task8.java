85b94593107ec67d3b788318914496b8001f2ab7
KolaYAndr <nikolay_andreev02@mail.ru>
2022-11-15 17:42:06 +0300

    Task8 extended сделал

--- src/ru/croc/task8/Task8.java
+++ src/ru/croc/task8/Task8.java
@@ -9,11 +9,21 @@ public class Task8 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter country: ");
+        String country = scanner.next();
+        System.out.print("Enter language code: ");
+        String language = scanner.next();
         System.out.print("Enter a double: ");
         try {
             Double prize = scanner.nextDouble();
-            NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
-            System.out.println("Result: " + format.format(prize));
+            if (country != null & language != null) {
+                Locale locale = new Locale(language, country);
+                NumberFormat format = NumberFormat.getCurrencyInstance(locale);
+                System.out.println("Result: " + format.format(prize));
+            } else {
+                NumberFormat format = NumberFormat.getCurrencyInstance(Locale.getDefault());
+                System.out.println("Result: " + format.format(prize));
+            }
         } catch (InputMismatchException e) {
             System.err.println(e.getMessage() + " invalid number");
         }
