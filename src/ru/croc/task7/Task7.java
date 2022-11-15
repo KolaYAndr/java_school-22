07f1c1409a4959f62fc00987720f087470504a52
KolaYAndr <nikolay_andreev02@mail.ru>
2022-11-15 17:08:28 +0300

    Task7 сделал

--- src/ru/croc/task7/Task7.java
+++ src/ru/croc/task7/Task7.java
@@ -9,28 +9,32 @@ public class Task7 {
 
     public static void main(String[] args) {
         Position pos0 = new Position("g8");
-        Position pos1 = new Position(5, 7);
-        System.out.println(pos1);
-//
-//        Position pos2 = new Position(5, 6);
-//        List<Position> poses = List.of(pos0, pos1, pos2);
-//        try {
-//            moveByKnight(poses);
-//        }
-//        catch (IllegalMoveException e){
-//            System.out.println(e.getMessage());
-//        }
+        Position pos1 = new Position("e7");
+        Position pos2 = new Position("e8");
+
+        System.out.println(moveByKnight(pos0, pos1, pos2));
     }
 
-    public static void moveByKnight(List<Position> positions) throws IllegalMoveException {
+    public static void move(List<Position> positions) throws IllegalMoveException {
         for (int i = 0; i < positions.size() - 1; i++) {
             Position currentPos = positions.get(i);
             Position nextPos = positions.get(i + 1);
-            boolean flag = (Math.abs(currentPos.getX() - nextPos.getX()) == 1 & Math.abs(currentPos.getY() - nextPos.getY()) == 2 | Math.abs(currentPos.getX() - nextPos.getX()) == 2 & Math.abs(currentPos.getY() - nextPos.getY()) == 1);
+            boolean flag = (Math.abs(currentPos.getLetter() - nextPos.getLetter()) == 1 & Math.abs(currentPos.getDigit() - nextPos.getDigit()) == 2 | Math.abs(currentPos.getLetter() - nextPos.getLetter()) == 2 & Math.abs(currentPos.getDigit() - nextPos.getDigit()) == 1);
             if (!flag) {
                 throw new IllegalMoveException(currentPos, nextPos);
             }
         }
+    }
+
+    public static String moveByKnight(Position... positions){
+        List<Position> poses = List.of(positions);
+        try {
+            move(poses);
+        }
+        catch (IllegalMoveException e){
+            return e.getMessage();
+        }
+        return "Приемлемо";
     }
 
 }
--- src/ru/croc/task7/classpackage/Position.java
+++ src/ru/croc/task7/classpackage/Position.java
@@ -3,45 +3,46 @@ package ru.croc.task7.classpackage;
 import java.util.List;
 
 public class Position {
-    private int x;
-    private int y;
-    public static final List<Character> list = List.of('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h');
+    private int letter;
+    private int digit;
+    public static final List<Character> letters = List.of('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h');
 
 
-    public Position(int x, int y) {
+    public Position(int letter, int digit) {
         try {
-            checkIfPositionIsOkThenSet(x, y);
+            checkThenSet(letter, digit);
         } catch (IllegalPositionException e) {
             System.out.println(e.getMessage());
         }
     }
 
     public Position(String pos) {
-        int y = list.indexOf(pos.charAt(0));
-        int x = pos.charAt(1);
-        new Position(x - '0' - 1, y);
+        int letter = letters.indexOf(pos.charAt(0));
+        int digit = pos.charAt(1) - '0' - 1;
+        try {
+            checkThenSet(letter, digit);
+        } catch (IllegalPositionException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-
-    private void checkIfPositionIsOkThenSet(int x, int y) throws IllegalPositionException {
-        if (x <= 7 & y <= 7 & x >= 0 & y >= 0) {
-            this.x = x;
-            this.y = y;
-        } else throw new IllegalPositionException(x, y);
+    private void checkThenSet(int letter, int digit) throws IllegalPositionException {
+        if (letter >= 0 & letter <= 7 & digit >= 0 & digit <= 7) {
+            this.digit = digit;
+            this.letter = letter;
+        } else throw new IllegalPositionException(letter, digit);
     }
 
-
-    public int getX() {
-        return x;
+    public int getLetter() {
+        return letter;
     }
 
-    public int getY() {
-        return y;
+    public int getDigit() {
+        return digit;
     }
 
     @Override
     public String toString() {
-        return list.get(x).toString() + (y + 1);
+        return letters.get(letter) + "" + (digit + 1);
     }
-
 }
