71ce393bf562a872559f435dc4a9dda15bcba52b
KolaYAndr <nikolay_andreev02@mail.ru>
2022-11-22 16:37:07 +0300

    Task11 в процессе

--- src/task11/classes/Server.java
+++ src/task11/classes/Server.java
@@ -3,11 +3,11 @@ package task11.classes;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.LinkedList;
+import java.util.List;
 
 public class Server {
     public static final int PORT = 2021;
-    static LinkedList<ServerReceiver> serverReceivers;
+    static List<ServerReceiver> serverReceivers;
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(PORT);
