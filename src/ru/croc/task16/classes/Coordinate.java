c2d2b1457a66f49649e76c4610f2a1bc6f537d4a
KolaYAndr <nikolay_andreev02@mail.ru>
2022-12-13 14:25:57 +0300

    Task 16 доделал

--- src/ru/croc/task16/classes/Coordinate.java
+++ src/ru/croc/task16/classes/Coordinate.java
@@ -4,15 +4,14 @@ public class Coordinate {
     private double latitude;
     private double longitude;
 
-    public Coordinate(double latitude, double longitude) {
+    public Coordinate(String coordinate) {
+        String[] coordinates = coordinate.split(", ");
+        double latitude = Double.parseDouble(coordinates[0]);
+        double longitude = Double.parseDouble(coordinates[1]);
         this.latitude = latitude;
         this.longitude = longitude;
     }
 
-    public Coordinate(String coordinate){
-        String[] coordinates = coordinate.split(", ");
-       this(Double.parseDouble(coordinates[0]), Double.parseDouble(coordinates[1]));
-    }
     int calculateDistanceTo(Coordinate coordinate) {
         return (int) Math.sqrt(Math.pow(this.longitude - coordinate.longitude, 2)
                 + Math.pow(this.latitude - coordinate.latitude, 2));
